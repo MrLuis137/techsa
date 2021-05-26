63f2a5dbbe828ff83244d80c540fd0e981471c8e
Josué <d4n11083@estudiantec.cr>
2021-05-26 00:37:00 -0600

    Cards para los dispositivos, y fix para que cuando se pongan varios se acomoden solos

--- techsa_frontend/src/app/app-routing.module.ts
+++ techsa_frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DevicesListingComponent } from './views/devices-listing/devices-listing.component';
 import { LoginComponent } from './views/login/login.component';
 import { TelephonyListingComponent } from './views/telephony-listing/telephony-listing.component';
 
@@ -7,7 +8,8 @@ import { TelephonyListingComponent } from './views/telephony-listing/telephony-l
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
-  {path:'telephony-listing', component:TelephonyListingComponent}
+  {path:'telephony-listing', component:TelephonyListingComponent},
+  {path:'devices-listing', component:DevicesListingComponent}
 ];
 
 @NgModule({
