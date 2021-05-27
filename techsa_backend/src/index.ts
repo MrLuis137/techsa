7a3235894cc16cba4ca71d12a080f28d1918d795
MrLuis137 <47502456+MrLuis137@users.noreply.github.com>
2021-05-27 01:50:13 -0600

    CRUD Clientes

--- techsa_backend/src/index.ts
+++ techsa_backend/src/index.ts
@@ -16,38 +16,51 @@ createConnection().then(async connection => {
     const servicioRepository = connection.getRepository(Servicio);
     const contratoRepository = connection.getRepository(Contrato);
 
-    /*const app = express();
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //                                  Cliente
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------
+    const app = express();
     app.use(express.json)
     
-    //Create user
+    //Nuevo Cliente
     app.post("/users" , async function (req: Request, res: Response) {
-        const user = await userRepository.create(req.body);
-        const results = await userRepository.save(user);
+        const user = await clienteRepository.create(req.body);
+        const results = await clienteRepository.save(user);
         return res.send(results);
         
     } )
 
-    //Get user
+    //Ver todos los usuarios
     app.get("/users", async function(req: Request, res: Response) {
-        const results = await userRepository.find();
+        const results = await clienteRepository.find();
         return res.send(results);
     });
     
-    //Get user by idcd
+    //Usuario por id
     app.get("/users/:id", async function(req: Request, res: Response) {
-        // here we will have logic to return user by id
+        const cliente = await clienteRepository.findOne(req.params.id);
+        return res.send(cliente);
     });
     
-    //Update user by id
+    //Actualizar usuario por id
     app.put("/users/:id", async function(req: Request, res: Response) {
-        // here we will have logic to update a user by a given user id
+        const user = await clienteRepository.findOne(req.params.id);
+        clienteRepository.merge(user, req.body)
+        const results = await clienteRepository.save(user)
+        return res.send(results);
     });
     
     //Delete user by id
     app.delete("/users/:id", async function(req: Request, res: Response) {
-        //
+        const results = await clienteRepository.delete(req.params.id);
+        return res.send(results);
     });
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //                                  Cliente
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------
     app.listen(3000);
-    console.log("Servidor iniciado")*/
+    console.log("Servidor iniciado")
 
 }).catch(error => console.log(error));
