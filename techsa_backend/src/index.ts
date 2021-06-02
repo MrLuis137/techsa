09bdf19bf07aebc8328d925d431431d914ed3622
Bayron03 <bayronbarrios25@gmail.com>
2021-06-01 22:16:59 -0600

    dispositivo
    new device

--- techsa_backend/src/index.ts
+++ techsa_backend/src/index.ts
@@ -6,21 +6,16 @@ import "reflect-metadata";
 import {createConnection, Repository} from "typeorm";
 import { Gerente } from './entity/Gerente';
 import {Dispositivo} from './entity/Dispositivo';
-import { Servicio } from './entity/Servicio';
+import {Servicio} from './entity/Servicio';
 import { Cliente } from './entity/Cliente';
 import { Contrato } from './entity/Contrato';
 import { AgenteVentas } from './entity/AgenteVentas';
-import { PlanInternet } from './entity/PlanInternet';
-import { PlanInternetPlanMovil } from './entity/PlanIntertnetPlanMovil';
-import { PlanMovil } from './entity/PlanMovil';
-import { PlanMovilDispositivo } from './entity/PlanMovilDispositivo';
 
 createConnection().then(async connection => {
     const clienteRepository = connection.getRepository(Cliente);
     const servicioRepository = connection.getRepository(Servicio);
     const contratoRepository = connection.getRepository(Contrato);
-    const planMovilRepository = connection.getRepository(PlanMovil);
-    const planMovilDispositivo = connection.getRepository(PlanMovilDispositivo);
+    const dispositivoRepository = connection.getRepository(Dispositivo);
 
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -67,40 +62,12 @@ createConnection().then(async connection => {
     //                                  Cliente
     //----------------------------------------------------------------------------------------------------------------------------------------------------------
     
-    //----------------------------------------------------------------------------------------------------------------------------------------------------------
-    //                                  Servicios
-    //----------------------------------------------------------------------------------------------------------------------------------------------------------
-    app.post("/planmovil", async function(req: Request, res: Response){
-        const servicio = await  servicioRepository.create({Nombre:'Plan_Movil'});
-        let planMovil = await planMovilRepository.create(req.body);
-        planMovil[0].IdServicio = servicio;
-        servicioRepository.save(servicio);
-        const result = planMovilRepository.save(planMovil);
-        res.send(result)
-    })
-
-    app.put("/planmovil:id"), async function (req:Request, res: Response) {
-        const PlanMovil = await planMovilRepository.findOne(req.params.id);
-        planMovilRepository.merge(PlanMovil, req.body);
-        const results = await planMovilRepository.save(PlanMovil)
-        return res.send(results)
-    }
-
-    app.get("/planmovil"), async function (req: Request, res: Response){
-        const results = planMovilRepository.find();
-        res.send(results);
-    }
-
-    app.get("/planmovil:id"), async function (req: Request, res: Response){
-        const results = planMovilRepository.findOne(req.params.id);
-        res.send(results);
-    }
-
-    //Falta delete
-
-    //----------------------------------------------------------------------------------------------------------------------------------------------------------
-    //                                  Servicios
-    //----------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Nuevo dispositivo
+    app.post("/devices-managment" , async function (req: Request, res: Response) {
+        const device = await dispositivoRepository.create(req.body);
+        const results = await dispositivoRepository.save(device);
+        return res.send(results);
+    } )
     
     app.listen(3000);
     console.log("Servidor iniciado")
--- techsa_frontend/src/app/views/new-device-form/new-device-form.component.ts
+++ techsa_frontend/src/app/views/new-device-form/new-device-form.component.ts
@@ -7,24 +7,13 @@ import { FormGroup, ReactiveFormsModule, FormBuilder, FormControl } from '@angul
   styleUrls: ['./new-device-form.component.css']
 })
 export class NewDeviceFormComponent implements OnInit {
-  newDeviceForm: FormGroup
+  
   constructor(private builder:FormBuilder) { 
-    this.newDeviceForm =  this.builder.group({
-      Modelo: [''],
-      Marca: [''],
-      Ram: [''],
-      Almacenamiento: [''],
-      Precio: [''],
-      Cantidad: [''],
-      Imagen: ['']
-    })
+    
 
   }
   
   ngOnInit(): void {
-  }
-  add(values){
-    console.log(values)
   }
   
 }
