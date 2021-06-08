4f9a54c2d8985e118feb769eb5e4a6e595a61e79
Josué <d4n11083@estudiantec.cr>
2021-06-07 22:19:22 -0600

    Lista de Dispositivos Lista, ya se muestran, falta lo de la imagen

--- techsa_frontend/src/app/views/devices-listing/devices-listing.component.ts
+++ techsa_frontend/src/app/views/devices-listing/devices-listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Dispositivo } from '../../models/Dispositivo';
+import { DeviceService } from '../../services/device.service';
 
 
 @Component({
@@ -9,12 +10,37 @@ import { Dispositivo } from '../../models/Dispositivo';
   styleUrls: ['./devices-listing.component.css']
 })
 export class DevicesListingComponent implements OnInit {
+  deviceList:Dispositivo[] = [];
 
-  constructor(private car :CartService) { }
+  constructor(private car :CartService, private deviceService:DeviceService) { }
 
   ngOnInit(): void {
+    this.refresh();
     this.addToCart()
+    console.log(this.deviceList);
   }
+
+  async refresh(){
+    const data = await this.deviceService.getDispositivoAll();
+    
+    data.forEach(element => {
+      let tempDevice = new Dispositivo;
+      tempDevice.Id = element.Id;
+      tempDevice.Modelo = element.Modelo;
+      tempDevice.Marca = element.Marca;
+      tempDevice.Color = element.Color;
+      tempDevice.Camara = element.Camara;
+      tempDevice.Ram = element.Ram;
+      tempDevice.Almacenamiento = element.Almacenamiento;
+      tempDevice.Precio = element.Precio;
+      tempDevice.Cantidad = element.Cantidad;
+      tempDevice.Imagen = element.Imagen;
+
+      this.deviceList.push(tempDevice);
+    });
+
+  }
+
 
   addToCart(){
     console.log("HOLA");
@@ -23,6 +49,7 @@ export class DevicesListingComponent implements OnInit {
     p.Camara = 5
     p.Color = "rojo"
     p.Marca = "Samsung"
+
     //this.car.addPhone(p)
   }
 
