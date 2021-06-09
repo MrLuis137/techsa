c04fef8233f438cda3345add14bd6afbc94e4fd7
Josué <d4n11083@estudiantec.cr>
2021-06-08 22:33:05 -0600

    form del login y cambio en los botones de Info y adquisición

--- techsa_frontend/src/app/views/login/login.component.ts
+++ techsa_frontend/src/app/views/login/login.component.ts
@@ -10,12 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email:string;
+  password:string;
+
 
   constructor () {}
 
 
   ngOnInit(): void {
   
+  }
+
+  login(){
+    console.log(this.email);
+    console.log(this.password);
   }
 
   
