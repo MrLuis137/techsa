b96a0aee2d3bdcc0e239aa1d0c297f63fbdd8f77
Josué <d4n11083@estudiantec.cr>
2021-06-11 19:23:34 -0600

    docu plan fijo listing y planfijo.service

--- techsa_frontend/src/app/services/planfijo.service.ts
+++ techsa_frontend/src/app/services/planfijo.service.ts
@@ -15,7 +15,6 @@ export class PlanfijoService {
   //Crea el request al backend
   private async request(method: string, url:string, data?:any, responseType?:any){
     
-    //console.log('request' + JSON.stringify(data));
     const result = this.http.request(method,url,{
       body:data,
       responseType:responseType || 'json',
@@ -28,27 +27,43 @@ export class PlanfijoService {
     });
   }
 
-  //Retorna todos los planes fijos de la base de datos 
+  //GetPlanFijoAll
+  //Le dice al backend que le envíe la lista de planes fijos 
+  //Retorna false si hay algún problema en el request
   async getPlanFijoAll(){
-    return this.request('get', `${baseUrl}/planfijo`);
+    try {
+      return await this.request('get', `${baseUrl}/planfijo`);
+    } catch (error) {
+      return false;
+    }
   }
 
-  //Busca un plan fijo por id 
+  //getPlanFijobyId
+  //Solicita al back end que retorne un plan fijo, buscandolo por el id
+  //Retorna false si hay algún problema en el request
   async getPlanFijobyId(id:string){
     return this.request('get', `${baseUrl}/planfijo/${id}`);
   }
 
-  //Guarda un plan fijo en la base de datos
+  //createPlanFijo
+  //Envía el cliente un nuevo plan fijo 
+  //Retorna 
   createPlanFijo( planFijo:PlanFijo ){
     console.log('createPlanfijo' + JSON.stringify(planFijo));
     return this.request('post', `${baseUrl}/planfijo`, planFijo);
   }
 
-  //Borra un plan fijo de la base de datos por id 
+  //deletePlanFijo
+  //solicita al backend que elimine un plan fijo, buscandolo por el id
+  //Retorna 
   deletePlanFijo( id:string ){
     return this.request('delete', `${baseUrl}/planfijo/${id}`, null, 'text');
   }
 
+
+  //updatePlanFijo
+  //Envía al backend un producto y le pide que lo actualice en la base de datos 
+  //Retorna 
   updatePlanFijo(id:string, planFijo:PlanFijo){
     console.log("servicio updateLandlineService ")
     return this.request('put', `${baseUrl}/planfijo/${id}`, planFijo,'text');
--- techsa_frontend/src/app/views/telephony-listing/telephony-listing.component.ts
+++ techsa_frontend/src/app/views/telephony-listing/telephony-listing.component.ts
@@ -3,6 +3,7 @@ import { PlanfijoService } from '../../services/planfijo.service';
 import { PlanFijo } from '../../models/PlanFijo';
 import { CartService } from '../../services/cart.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-telephony-listing',
@@ -15,7 +16,7 @@ export class TelephonyListingComponent implements OnInit {
 
   telephonyListingData:PlanFijo[] = []  //Lista de planes fijos que se muestran en el 
 
-  constructor(public planFijoService:PlanfijoService, public router:Router) { }
+  constructor(public planFijoService:PlanfijoService, public router:Router, public auth:AuthService) { }
 
 
   //Función que se ejecuta cuando secarga la página
@@ -28,28 +29,37 @@ export class TelephonyListingComponent implements OnInit {
   //guarda en la lista de telephonyListingData
   async refresh(){
     const data = await this.planFijoService.getPlanFijoAll(); //Pide la lista al backend
-    //Por cada elemento de la data, crea un nuevo planFijo 
-    data.forEach(element => {
-      let newPlanFijo = new PlanFijo;
-      newPlanFijo.ID = element.ID;
-      newPlanFijo.NombrePlan = element.NombrePlan;
-      newPlanFijo.IdServicio = element.idServicioId;
-      newPlanFijo.Minutos = element.Minutos;
-      newPlanFijo.FijoTechsa = element.FijoTechsa;
-      newPlanFijo.FijoOperador = element.FijoOperador;
-      newPlanFijo.MovilCualquiera = element.MovilCualquiera;
-      newPlanFijo.PrecioMensual = element.PrecioMensual;
-      this.telephonyListingData.push(newPlanFijo);      
-    });
+    //Por cada elemento de la data, crea un nuevo planFijo
+    if (data != false){
+      data.forEach(element => {   //Itera por cada elemento en data 
+        let newPlanFijo = new PlanFijo;
+        newPlanFijo.ID = element.ID;
+        newPlanFijo.NombrePlan = element.NombrePlan;
+        newPlanFijo.IdServicio = element.idServicioId;
+        newPlanFijo.Minutos = element.Minutos;
+        newPlanFijo.FijoTechsa = element.FijoTechsa;
+        newPlanFijo.FijoOperador = element.FijoOperador;
+        newPlanFijo.MovilCualquiera = element.MovilCualquiera;
+        newPlanFijo.PrecioMensual = element.PrecioMensual;
+        this.telephonyListingData.push(newPlanFijo);      
+      });
+    }else{
+      alert("No se pudieron cargar los planes fijos \n Intentelo de nuevo");
+    }
   }
 
   //addToCart
   //Añade un planFijo al carrito
   addToCart(planFijo:PlanFijo){
-    console.log(planFijo);
+    if (this.auth.loggedIn) {   //Si ya está logueado, puede adquirir el servicio 
+      console.log("TelephonyListing:addtoCart:Añadiendo Producto al carrito");
+      console.log(planFijo);
 
-    alert("");
-
+      //Añadir al carrito
+    }else{  //Si no está logueado recibe un mensaje de error
+      if(confirm("Debe inicar sesión para adquirir el producto \n ¿Desea ir a la página de LogIn?")){
+        this.router.navigate(['login']);
+      }
+    }
   }
-
 }
